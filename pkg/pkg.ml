ed78c45bce13b9af2acdf6b3a64f179480b4cdaa
Marshall Roch <mroch@fb.com>
2017-09-26 14:38:54 -0700

    configure topkg to use github source tar.gz

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -9,9 +9,13 @@ let opams =
   let lint_deps_excluding = Some ["ocb-stubblr"; "ounit"; "oUnit"] in
   [Pkg.opam_file ~lint_deps_excluding "dtoa.opam"]
 
+let distrib = Pkg.distrib
+  ~uri:"https://github.com/flowtype/ocaml-dtoa/archive/$(VERSION_NUM).tar.gz"
+  ()
+
 let () =
   Pkg.describe "dtoa"
-  ~build ~opams
+  ~build ~opams ~distrib
   ~licenses:[ Pkg.std_file "LICENSE" ]
   @@ fun c ->
   Ok [ Pkg.mllib "src/dtoa.mllib";
